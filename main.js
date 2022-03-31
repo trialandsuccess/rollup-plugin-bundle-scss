7a9cd2b7656aa95950f70e46ff7ec28ca9043e40
Robin <robinvandernoord@gmail.com>
2022-03-31 10:21:49 +0200

    fix module and add optional prettier

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 import fs from "fs";
 import * as sass from "node-sass";
-import {green, underline} from "colorette";
+import { green, underline } from "colorette";
 import * as path from "path";
 
 function change_ext(file, ext) {
-    return path.format({...path.parse(file), base: '', ext: `.${ext}`});
+    return path.format({ ...path.parse(file), base: "", ext: `.${ext}` });
 }
 
 export function scss(files, name) {
     return {
-        name: 'bundle-scss',
+        name: "bundle-scss",
         async buildStart() {
             for (let file of files) {
                 this.addWatchFile(file);
@@ -23,28 +23,25 @@ export function scss(files, name) {
             }
 
             // hacky way to combine scss files:
-            const imports = files.map(f => `@import "${f}";`).join("\n");
+            const imports = files.map((f) => `@import "${f}";`).join("\n");
 
-            sass.render({
-                data: imports,
+            sass.render(
+                {
+                    data: imports,
 
-                outputStyle: process.env.BUILD === "production" ? "compressed" : "expanded",
-                // includePaths: "./node_modules",
-                indentType: "spaces",
-                indentWidth: 4,
+                    outputStyle: process.env.BUILD === "production" ? "compressed" : "expanded",
+                    // includePaths: "./node_modules",
+                    indentType: "spaces",
+                    indentWidth: 4,
+                },
+                (_, css) => {
+                    const out_name = change_ext(name, "css");
 
-            }, (_, css) => {
-                const out_name = change_ext(name, 'css');
+                    fs.writeFileSync(`${dir}/${out_name}`, css.css);
 
-                fs.writeFileSync(`${dir}/${out_name}`, css.css);
-
-                console.log(
-                    green(
-                        underline(out_name),
-                    ),
-                    green("compiled succesfully."),
-                );
-            });
-        }
+                    console.log(green(underline(out_name)), green("compiled succesfully."));
+                }
+            );
+        },
     };
 }
